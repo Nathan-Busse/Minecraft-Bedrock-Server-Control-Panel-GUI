73f658e4385c360f79f63458e95e4d81985711c9
Nathan Busse <82398683+Nathan-Busse@users.noreply.github.com>
2022-02-16 20:46:12 +0200

    Delete MCBE Server/MCBE control gui directory

--- MCBE Server/MCBE control gui/update.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-
-url = 'https://minecraft.azureedge.net/bin-win/bedrock-server-1.18.11.01.zip'
-r = requests.get(url, allow_redirects=True)
-
-open('bedrock-server-1.18.11.01.zip', 'wb').write(r.content)
