4f29a4bf8520e7dfabcfab372fe204071c2e758b
Nathan Busse <82398683+Nathan-Busse@users.noreply.github.com>
2022-02-22 22:45:21 +0200

    Update update.py

--- MCBE Server/MCBE Control Panel/update.py
+++ MCBE Server/MCBE Control Panel/update.py
@@ -1,7 +1,7 @@
 import requests
 
 
-url = 'https://minecraft.azureedge.net/bin-win/bedrock-server-1.18.11.01.zip'
+url = 'https://minecraft.azureedge.net/bin-win/bedrock-server-1.18.12.01.zip'
 r = requests.get(url, allow_redirects=True)
 
 open('bedrock-server-1.18.11.01.zip', 'wb').write(r.content)
