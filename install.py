c110604f94c5016dfa3b66f580a3c7a7cf57959a
Nathan Busse <82398683+Nathan-Busse@users.noreply.github.com>
2022-10-21 15:54:53 +0200

    Add files via upload

---
+++ install.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+
+print('\033[2;32;43m Installing python packages...')	
+
+os.system('cmd /c "pip install pathlib"')
+os.system('cmd /c "pip install shutil"')
+os.system('cmd /c "pip install re"')
+os.system('cmd /c "pip install subprocess"')
+os.system('cmd /c "pip install contextlib"')
+os.system('cmd /c "pip install threading"')
+os.system('cmd /c "pip install inspect"')
+os.system('cmd /c "pip install requests"')
+os.system('cmd /c "pip install pyautogui"')
+os.system('cmd /c "pip install PySimpleGUI"')
+
+dirname = os.path.dirname("Prepare")
+file_path = 'c:/users/' + os.getlogin()
+filename = os.path.join(dirname, file_path,'./Documents/MCBE Server/dog' )
+
+os.mkdir(filename)
+
+# mode
+mode = 0o666
+  
+# Path
+path = os.path.join(filename)
+  
+print("Directory '% s' created" )
+
+sys.exit()
